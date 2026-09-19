c07ff587b3a2fcb1e6393a13bc1a8f2404df703e
mahfuzeee <45316693+mahfuzeee@users.noreply.github.com>
2026-09-19 16:35:12 +0600

    Implement recursive Fibonacci function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -9,6 +9,16 @@ function fibs(n) {
   return result;
 }
 
-function fibsRec(n) {}
+function fibsRec(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const previous = fibsRec(n - 1);
+  const lastIndex = previous.length - 1;
+
+  return [...previous, previous[lastIndex] + previous[lastIndex - 1]];
+}
 
 console.log(fibs(8));
+console.log(fibsRec(8));
